72d568e50e8bdd9b202eb851a1e9e4539ee2e1fa
lordberre-server <jakerlock@gmail.com>
2023-05-21 20:43:08 +0200

    arg fix

--- docker_run.sh
+++ docker_run.sh
@@ -3,5 +3,5 @@ source .env
 if [[ $FOREGROUND == true ]]; then
     docker run --network=hlds_network --cpuset-cpus="0" --ip $HLDS_IP -it --restart=always --name $CONTAINER -p $HLDS_PORT:$HLDS_PORT/udp -p $HLDS_PORT:$HLDS_PORT/tcp -p $HLDS_VACPORT:$HLDS_VACPORT/udp $CONTAINER
 else
-    docker run -it --network=hlds_network --cpuset-cpus="0" --ip $HLDS_IP -d --restart=always --name $CONTAINER -p $HLDS_PORT:$HLDS_PORT/udp -p $HLDS_PORT:$HLDS_PORT/tcp -p $HLDS_VACPORT:$HLDS_VACPORT/udp $CONTAINER
+    docker run --network=hlds_network --cpuset-cpus="0" --ip $HLDS_IP -d --restart=always --name $CONTAINER -p $HLDS_PORT:$HLDS_PORT/udp -p $HLDS_PORT:$HLDS_PORT/tcp -p $HLDS_VACPORT:$HLDS_VACPORT/udp $CONTAINER
 fi
